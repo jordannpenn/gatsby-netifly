2fc3a134197e9d6b206245681b0db4a8a09c0741
Jordan Penn <jordanpenn@Jordans-Mini.home>
2021-06-18 11:42:05 -0400

    Secondt commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -139,7 +139,7 @@ const IndexPage = () => {
         </span>
       </h1>
       <p style={paragraphStyles}>
-         <code style={codeStyles}>src/pages/index.js</code> to see this page
+        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
         update in real-time.{" "}
         <span role="img" aria-label="Sunglasses smiley emoji">
           😎
